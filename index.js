a51d0f461628bca36a8bd62452844f326602c836
Matheus <matheus08.pimentel@gmail.com>
2020-01-27 19:35:11 -0300

    Add the remain HTTP request missing

--- index.js
+++ index.js
@@ -6,21 +6,68 @@ server.use(express.json());
 
 const projects = [];
 
-// GET:
-
-server.get('/projects', (req, res) => {
-  return res.json(projects);
-})
 
 // POST:
 
 server.post('/projects', (req, res) => {
-  const { title, tasks } = req.body;
+  const { id, title } = req.body;
 
-  projects.push(title, tasks);
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
 
   return res.json(projects);
 
+});
+
+
+// GET:
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+
+// PUT:
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(projects);
+});
+
+
+// DELETE:
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id);
+
+  return res.send();
+});
+
+
+// POST TASKS:
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
 })
 
 server.listen(3000);
